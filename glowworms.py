87c69a56f3c875ac72b0849f53b83255f306009c
Adam <adamkosinski631@gmail.cm>
2024-05-23 04:10:57 -0400

    Command line arg for hanging/normal sign

--- glowworms.py
+++ glowworms.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 import random
 from time import sleep
+import sys
 
 # r = Tk()
 # r.withdraw()
@@ -16,7 +17,7 @@ def copy_to_clipboard(text):
 
 def generate_glowworm_row(hanging_sign=False):
 	symbols = [" ", ". ", "* ", "+ "]
-	weights = [30, 6, 3, 1]
+	weights = [30 if hanging_sign else 60, 6, 3, 1]
 	widths = {" ": 2, ". ": 1+2, "* ": 2+2, "+ ": 3+2}
 
 	sign_width = 30 if hanging_sign else 45
@@ -34,8 +35,10 @@ def generate_glowworm_row(hanging_sign=False):
 
 
 if __name__ == "__main__":
+	hanging_sign = len(sys.argv) > 1
+	print("Hanging Sign" if hanging_sign else "Normal Sign")
 	while(True):
-		row = generate_glowworm_row(True)
+		row = generate_glowworm_row(hanging_sign)
 		print(row)
 		copy_to_clipboard(row)
 		sleep(0.1)
