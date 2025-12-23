51cfda037bb362d1b973d9334a04429cf2294245
Adam <adamkosinski631@gmail.cm>
2025-12-23 16:58:45 -0500

    Cool letter shuffling thing

---
+++ Shuffled-Letters/shuffle.js
@@ -0,0 +1,26 @@
+const textarea = document.getElementById("input");
+const output = document.getElementById("output");
+
+textarea.addEventListener("input", () => {
+  output.textContent = processText(textarea.value);
+});
+
+function processText(text) {
+  return text.replaceAll(/\w+/g, (s) => shuffleInnerLetters(s));
+}
+
+function shuffleInnerLetters(word) {
+  if (word.length <= 3) return word;
+  const inner = word.substring(1, word.length - 1);
+  return word[0] + shuffle(inner) + word[word.length - 1];
+}
+
+function shuffle(s) {
+  const letters = s.split("");
+  let out = "";
+  while (letters.length > 0) {
+    const i = Math.floor(letters.length * Math.random());
+    out += letters.splice(i, 1)[0];
+  }
+  return out;
+}
